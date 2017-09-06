073f4af23909ec1ba16610619ad6dec46f3f1ce1
anzcarroll <anzcarroll@gmail.com>
2017-09-05 21:14:51 -0400

    added form to create new artist

---
+++ client/src/components/NewArtist.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class NewArtist extends Component {
+    constructor() {
+        super();
+        this.state = {
+            NewArtist: {
+                name: "",
+                nationality: "",
+                photo_url: "",
+            }
+        }
+    }
+    _handleSumbit = (e) => {
+e.preventDefault();
+console.log("lets create a newbby")
+    }
+    _handleChange = (e) => {
+        const attributeValue = e.target.value;
+        const attributeName =  e.target.name;
+        const newState = {...this.state};
+        newState.NewArtist[attributeName] = attributeValue
+        this.setState(newState)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this._handleSubmit}>
+                    <input type="text" name="name" placeholder="artist name" onChange={this._handleChange}/>
+                    <input type="text" name="nationality" placeholder="nationality" onChange={this._handleChange}/>
+                    <input type="text" name="photo_url" placeholder="insert a url for photo" onChange={this._handleChange}/>
+                    <input type="submit" value="Add Artist"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default NewArtist;
